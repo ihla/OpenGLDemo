20847e1ea9cb11d605ffaf82a8b409e40cb4ede3
Lubos Ilcik <lubos.ilcik@gmail.com>
2013-03-12 16:08:04 +0100

    - displaying triangle on screen

--- OpenGLDemo/src/co/joyatwork/opengldemo/GLRenderer.java
+++ OpenGLDemo/src/co/joyatwork/opengldemo/GLRenderer.java
@@ -4,19 +4,46 @@ import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
 import android.opengl.GLSurfaceView.Renderer;
+import android.opengl.GLU;
 
 public class GLRenderer implements Renderer {
 
+	private Triangle 	triangle;	// the triangle to be drawn
+	 
+	/** Constructor */
+	public GLRenderer() {
+		this.triangle = new Triangle();
+	}
+	
 	@Override
 	public void onDrawFrame(GL10 gl) {
-		// TODO Auto-generated method stub
-		
+		// clear Screen and Depth Buffer
+		gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
+ 
+		// Reset the Modelview Matrix
+		gl.glLoadIdentity();
+ 
+		// Drawing
+		gl.glTranslatef(0.0f, 0.0f, -5.0f);		// move 5 units INTO the screen
+												// is the same as moving the camera 5 units away
+		triangle.draw(gl);						// Draw the triangle
 	}
 
 	@Override
 	public void onSurfaceChanged(GL10 gl, int width, int height) {
-		// TODO Auto-generated method stub
-		
+		if(height == 0) { 						//Prevent A Divide By Zero By
+			height = 1; 						//Making Height Equal One
+		}
+ 
+		gl.glViewport(0, 0, width, height); 	//Reset The Current Viewport
+		gl.glMatrixMode(GL10.GL_PROJECTION); 	//Select The Projection Matrix
+		gl.glLoadIdentity(); 					//Reset The Projection Matrix
+ 
+		//Calculate The Aspect Ratio Of The Window
+		GLU.gluPerspective(gl, 45.0f, (float)width / (float)height, 0.1f, 100.0f);
+ 
+		gl.glMatrixMode(GL10.GL_MODELVIEW); 	//Select The Modelview Matrix
+		gl.glLoadIdentity(); 					//Reset The Modelview Matrix
 	}
 
 	@Override
--- OpenGLDemo/src/co/joyatwork/opengldemo/Triangle.java
+++ OpenGLDemo/src/co/joyatwork/opengldemo/Triangle.java
@@ -4,6 +4,9 @@ import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.FloatBuffer;
 
+//The GL10 interface contains the Java(TM) programming language bindings for OpenGL(R) ES 1.0 core functionality:
+import javax.microedition.khronos.opengles.GL10;
+
 public class Triangle {
 	private FloatBuffer vertexBuffer;	// buffer holding the vertices
 	 
@@ -13,6 +16,10 @@ public class Triangle {
 			 0.0f,  0.5f,  0.0f			// V3 - third vertex
 	};
  
+	/**
+	 * Constructs triangle out of 3 points.
+	 * Stores vertexes of points in java.nio.FloatBuffer for direct use by OpenGL.
+	 */
 	public Triangle() {
 		// a float has 4 bytes so we allocate for each coordinate 4 bytes
 		ByteBuffer vertexByteBuffer = ByteBuffer.allocateDirect(vertices.length * 4);
@@ -26,5 +33,31 @@ public class Triangle {
  
 		// set the cursor position to the beginning of the buffer
 		vertexBuffer.position(0);
+	}
+
+	/**
+	 * Draws triangle.
+	 * @param gl
+	 */
+	public void draw(GL10 gl) {
+		// we need to enable OpenGL to read from FloatBuffer and understand that is a triangle there
+		gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
+
+		// set the colour for the triangle
+		// the values of the rgb are floats and are between 0.0 and 1.0
+		gl.glColor4f(0.0f, 1.0f, 0.0f, 0.5f);
+
+		// Point to our vertex buffer
+		// glVertexPointer(size, type, stride, pointer) will tell OpenGL to use the vertexBuffer
+		//
+		// size specifies the number of coordinates per vertex and type the data type of the coordinates
+		// stride is the offset between values in array
+		gl.glVertexPointer(3, GL10.GL_FLOAT, 0, vertexBuffer);
+
+		// Draw the vertices as triangle strip
+		gl.glDrawArrays(GL10.GL_TRIANGLE_STRIP, 0, vertices.length / 3);
+
+		//Disable the client state before leaving
+		gl.glDisableClientState(GL10.GL_VERTEX_ARRAY);
 	}
 }
